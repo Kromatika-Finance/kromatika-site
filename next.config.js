8b04c2a06ac98455159be3c111cf29f27e6ddfe5
stefan_a <stefan_apostolovski97@hotmail.com>
2022-07-25 17:12:23 +0200

    terms of use fix

--- next.config.js
+++ next.config.js
@@ -1,23 +1,26 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
+const path = require("path");
 
 const nextConfig = {
-	reactStrictMode: true,
-	optimizeFonts: true,
-	assetPrefix: './',
-	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')],
-	},
-	images: {
-		domains: ['assets'],
-		formats: ['image/avif', 'image/webp'],
-		dangerouslyAllowSVG: true,
-		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-		minimumCacheTTL: 60,
-		loader: 'akamai',
-		path: '',
-	},
+  reactStrictMode: true,
+  optimizeFonts: true,
+  assetPrefix: ".",
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    domains: ["assets"],
+    formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    minimumCacheTTL: 60,
+    loader: "akamai",
+    path: "",
+    compilerOptions: {
+      baseUrl: "src",
+    },
+  },
 };
 
 module.exports = nextConfig;
